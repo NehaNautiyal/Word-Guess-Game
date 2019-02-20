ee226043b75e6322e9b64cb94cde31d75865e7b1
Neha Nautiyal <nehahaha@gmail.com>
2019-02-20 17:01:45 -0500

    game and style fixes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,55 @@
 
 //Define the array with words to choose from
 var chemWords = ["oxygen", "nitrogen", "carbon", "hydrogen", "helium", "sodium", "aluminum", "silver", "uranium", "copper",
-"potassium", "sulfur", "krypton", "radon", "iron", "gold"]
-var blanks = [];
+    "potassium", "sulfur", "krypton", "radon", "iron", "gold"]
+
 
 
 //Initial scores
 var wins = 0;
 var losses = 0;
+
+var randomNumber = Math.floor(Math.random() * chemWords.length);
+var randomWord = chemWords[randomNumber];
+
+var showImage = function() {
+
+    var imageDetails = document.getElementById("image");
+
+    if (randomWord === chemWords[0]) {
+        imageDetails.setAttribute("src", "assets/images/oxygen.JPG");
+    } else if (randomWord === chemWords[1]) {
+        imageDetails.setAttribute("src", "assets/images/nitrogen.JPG");
+    } else if (randomWord === chemWords[2]) {
+        imageDetails.setAttribute("src", "assets/images/carbon.JPG");
+    } else if (randomWord === chemWords[3]) {
+        imageDetails.setAttribute("src", "assets/images/hydrogen.JPG");
+    } else if (randomWord === chemWords[4]) {
+        imageDetails.setAttribute("src", "assets/images/helium.JPG");
+    } else if (randomWord === chemWords[5]) {
+        imageDetails.setAttribute("src", "assets/images/sodium.JPG");
+    } else if (randomWord === chemWords[6]) {
+        imageDetails.setAttribute("src", "assets/images/aluminum.JPG");
+    } else if (randomWord === chemWords[7]) {
+        imageDetails.setAttribute("src", "assets/images/silver.JPG");
+    } else if (randomWord === chemWords[8]) {
+        imageDetails.setAttribute("src", "assets/images/uranium.JPG");
+    } else if (randomWord === chemWords[9]) {
+        imageDetails.setAttribute("src", "assets/images/copper.JPG");
+    } else if (randomWord === chemWords[10]) {
+        imageDetails.setAttribute("src", "assets/images/potassium.JPG");
+    } else if (randomWord === chemWords[11]) {
+        imageDetails.setAttribute("src", "assets/images/sulfur.JPG");
+    } else if (randomWord === chemWords[12]) {
+        imageDetails.setAttribute("src", "assets/images/krypton.JPG");
+    } else if (randomWord === chemWords[13]) {
+        imageDetails.setAttribute("src", "assets/images/radon.JPG");
+    } else if (randomWord === chemWords[14]) {
+        imageDetails.setAttribute("src", "../images/iron.JPG");
+    } else if (randomWord === chemWords[15]) {
+        imageDetails.setAttribute("src", "../images/gold.JPG");
+    }
+}
 
 
 
@@ -18,16 +60,16 @@ document.onkeyup = function (event) {
 
         var numGuesses = 10;
         var wrongLetters = [];
+        var blanks = [];
         var wrongLetter = document.getElementById("incorrect");
-        var rightLetter = document.getElementById("rightLetters");
+        // var rightLetter = document.getElementById("rightLetters");
         var guessesLeft = document.getElementById("guessesRemaining");
 
         wrongLetter.textContent = "Incorrect Guesses: " + wrongLetters.join(" ");
         guessesLeft.textContent = numGuesses;
 
         //when a key is pressed, a random word from the array is chosen
-        var randomNumber = Math.floor(Math.random() * chemWords.length);
-        var randomWord = chemWords[randomNumber];
+       
         console.log(randomWord);
 
         //In order for the random chosen word to be displayed, need to know its length
@@ -46,7 +88,7 @@ document.onkeyup = function (event) {
 
         //Play the game
         document.onkeyup = function (event) {
-            var usersLetter = event.key;
+            var usersLetter = event.key.toLowerCase();
             console.log(usersLetter);
 
             //evalute the letter being pressed to guess
@@ -65,7 +107,9 @@ document.onkeyup = function (event) {
                     if (randomWord[j] === usersLetter) {
                         letterMatch = true;
                         console.log(letterMatch);
+                        // the user's letter becomes the letter in the blank array where it has made the match
                         blanks[j] = usersLetter;
+                        //display the array with the user's letter in the correct index position
                         wordLines.textContent = blanks.join(" ");
                     }
                 }
@@ -74,6 +118,7 @@ document.onkeyup = function (event) {
                     // add that letter to an array called wrongLetters
                     wrongLetters.push(usersLetter);
                     console.log(wrongLetters);
+                    //number of guesses goes down by 1
                     numGuesses--;
                     guessesLeft.textContent = numGuesses;
                     wrongLetter.textContent = "Incorrect Guesses: " + wrongLetters.join(" ");
@@ -93,15 +138,17 @@ document.onkeyup = function (event) {
                 refreshPage();
             }
 
-            var numWins = document.getElementById("wins");    
+            var numWins = document.getElementById("wins");
 
-            
+
             console.log("This is blanks array: " + blanks);
             console.log("This is wordLines array: " + wordLines);
-            
+
+
             if (blanks.includes(" ___ ") === false) {
                 wins++;
                 numWins.textContent = wins;
+                showImage();
                 alert("Congratulations! You guessed " + randomWord + " correctly! Press OK to play again.");
                 refreshPage();
             }
